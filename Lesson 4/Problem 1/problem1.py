7f97290863e3b5dc7423d258dadecb61ee81432f
shannonay0103 <43756247+shannonay0103@users.noreply.github.com>
2018-12-04 13:32:19 -0500

    Add files via upload

---
+++ Lesson 4/Problem 1/problem1.py
@@ -0,0 +1,3 @@
+new_score = round(new_score)
+new_score = score/4 * 100
+score = 2
---
+++ Lesson 4/Problem 3/problem3.py
@@ -0,0 +1,5 @@
+average = (english + math + history + science + production)/4
+english= 89
+math= 95
+history= 87
+science= 84
